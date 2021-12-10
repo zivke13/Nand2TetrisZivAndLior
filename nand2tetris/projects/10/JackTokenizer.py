a5d9fcf2398dc1c5da980a5080dec1b8bf16bfe5
ziv kohn <ziv.kohn41@gmail.com>
2021-12-10 18:50:19 +0200

    project10/jackTokenizer

--- nand2tetris/projects/10/JackTokenizer.py
+++ nand2tetris/projects/10/JackTokenizer.py
@@ -5,6 +5,20 @@ and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0
 Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
 """
 import typing
+import re
+KEYWORDS = ["class", "method", "function", "constructor", "int", "boolean",
+             "char", "void", "var", "static", "field", "let", "do", "if",
+             "else", "while", "return", "true", "false", "null", "this"]
+
+SYMBOLS = ["{", "}", "[", "]", "(", ")", ".", ",", ";", "+", "-", "*", "/",
+            "&", "|", "<", ">", "=", "~"]
+
+
+KEYWORD = "KEYWORD"
+SYMBOL = "SYMBOL"
+IDENTIFIER = "IDENTIFIER"
+INT_CONST = "INT_CONST"
+STRING_CONST = "STRING_CONST"
 
 
 class JackTokenizer:
@@ -21,7 +35,48 @@ class JackTokenizer:
         # Your code goes here!
         # A good place to start is:
         # input_lines = input_stream.read().splitlines()
-        pass
+        self.input_lines = input_stream.read().splitlines()
+        self.remove_spaces()
+        self.remove_backslash()
+        self.input_lines = [line for line in self.input_lines if line]
+        self.input_tokens = []
+        self.split_to_token()
+        self.i = 0
+        self.current = self.input_tokens[0]
+
+
+    def remove_spaces(self):
+        """
+        delete the spaces from the input lines
+        :return:
+        """
+        for i in range(0, len(self.input_lines), 1):
+            self.input_lines[i] = self.input_lines[i].strip()
+
+    def remove_backslash(self):
+        for i in range(0, len(self.input_lines), 1):
+            split_string = self.input_lines[i].split("//", 1)
+            self.input_lines[i] = split_string[0]
+            split_string = self.input_lines[i].split("/*", 1)
+            self.input_lines[i] = split_string[0]
+            split_string = self.input_lines[i].split("*", 1)
+            self.input_lines[i] = split_string[0]
+
+    def split_line_to_token(self, line):
+        identifiers = '\w+'
+        integers = '\d+'
+        strings = '\".*\"'
+        keywords = ('class|method|function|constructor|int|boolean|char|void|'
+                         'var|static|field|let|do|if|else|while|return|true|false|'
+                         'null|this')
+        symbols = '{|}|\[|\]|\(|\)|\.|,|;|\+|-|\*|\/|&|\||<|>|=|~'
+        composed = r'({}|{}|{}|{}|{})'.format(identifiers, integers, strings, keywords, symbols)
+        return re.findall(composed, line)
+
+    def split_to_token(self):
+        for line in self.input_lines:
+            split_token = self.split_line_to_token(line)
+            self.input_tokens = self.input_tokens + split_token
 
     def has_more_tokens(self) -> bool:
         """Do we have more tokens in the input?
@@ -29,6 +84,10 @@ class JackTokenizer:
         Returns:
             bool: True if there are more tokens, False otherwise.
         """
+        if self.i < len(self.input_tokens) - 1:
+            return True
+        return False
+
         # Your code goes here!
         pass
 
@@ -37,8 +96,8 @@ class JackTokenizer:
         This method should be called if has_more_tokens() is true. 
         Initially there is no current token.
         """
-        # Your code goes here!
-        pass
+        self.i += 1
+        self.current = self.input_tokens[self.i]
 
     def token_type(self) -> str:
         """
@@ -46,8 +105,20 @@ class JackTokenizer:
             str: the type of the current token, can be
             "KEYWORD", "SYMBOL", "IDENTIFIER", "INT_CONST", "STRING_CONST"
         """
-        # Your code goes here!
-        pass
+        if self.current in SYMBOLS:
+            return SYMBOL
+
+        if self.current in KEYWORDS:
+            return KEYWORD
+
+        if re.match("^\".*\"$", self.current):
+            return STRING_CONST
+
+        if re.match("^\d+$", self.current):
+            return INT_CONST
+
+        if re.match("^\w+$", self.current):
+            return IDENTIFIER
 
     def keyword(self) -> str:
         """
@@ -58,6 +129,10 @@ class JackTokenizer:
             "BOOLEAN", "CHAR", "VOID", "VAR", "STATIC", "FIELD", "LET", "DO", 
             "IF", "ELSE", "WHILE", "RETURN", "TRUE", "FALSE", "NULL", "THIS"
         """
+        if self.current in SYMBOLS:
+            return self.current.upper
+        else:
+            return None
         # Your code goes here!
         pass
 
@@ -68,6 +143,7 @@ class JackTokenizer:
             Should be called only when token_type() is "SYMBOL".
         """
         # Your code goes here!
+        return self.current
         pass
 
     def identifier(self) -> str:
@@ -76,7 +152,7 @@ class JackTokenizer:
             str: the identifier which is the current token.
             Should be called only when token_type() is "IDENTIFIER".
         """
-        # Your code goes here!
+        return self.current
         pass
 
     def int_val(self) -> int:
@@ -85,7 +161,7 @@ class JackTokenizer:
             str: the integer value of the current token.
             Should be called only when token_type() is "INT_CONST".
         """
-        # Your code goes here!
+        return self.current
         pass
 
     def string_val(self) -> str:
@@ -94,5 +170,5 @@ class JackTokenizer:
             str: the string value of the current token, without the double 
             quotes. Should be called only when token_type() is "STRING_CONST".
         """
-        # Your code goes here!
+        return self.current
         pass
